e42a6ae23dbe36e942782f50fd8fa62c6a2b9806
hexianWeb <hexianweb@gmail.com>
2024-08-18 18:45:21 +0800

    feat: 新增 海底颜色

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
 import getStarfield from "./src/getStarfield.js";
+import { Pane } from "https://cdn.jsdelivr.net/npm/tweakpane@4.0.3/dist/tweakpane.min.js";
 
+const pane = new Pane();
+
+const PARAMS = {
+  color: "#000",
+  size: 4.0,
+};
+
+// 创建一个对象来保存背景颜色
+const backgroundColor = { color: "#000000" };
+
+pane
+  .addBinding(PARAMS, "color", {
+    type: "color",
+    label: "Color",
+  })
+  .on("change", ({ value }) => {
+    mat.color.set(value);
+  });
+
+pane
+  .addBinding(PARAMS, "size", {
+    type: "number",
+    min: 0.1,
+    max: 10.0,
+    step: 0.1,
+    label: "Size",
+  })
+  .on("change", ({ value }) => {
+    pointsMat.uniforms.size.value = value;
+  });
+
+// 添加颜色控制项
+pane
+  .addBinding(backgroundColor, "color", {
+    type: "color",
+    label: "Background Color",
+  })
+  .on("change", ({ value }) => {
+    // 将颜色设置为Three.js场景的背景颜色
+    scene.background = new THREE.Color(value);
+  });
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
 camera.position.set(0, 0, 3.5);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(innerWidth, innerHeight);
@@ -18,22 +65,24 @@ const starSprite = textureLoader.load("./src/circle.png");
 const colorMap = textureLoader.load("./src/04_rainbow1k.jpg");
 const elevMap = textureLoader.load("./src/01_earthbump1k.jpg");
 const alphaMap = textureLoader.load("./src/02_earthspec1k.jpg");
+const earthMap = textureLoader.load("./src/00_earthmap1k.jpg");
+const hexagonMap = textureLoader.load("./src/hexagon-fill.png");
 
 const globeGroup = new THREE.Group();
 scene.add(globeGroup);
 
-const geo = new THREE.IcosahedronGeometry(1, 10);
-const mat = new THREE.MeshBasicMaterial({ 
+const geo = new THREE.IcosahedronGeometry(1, 3);
+const mat = new THREE.MeshBasicMaterial({
   color: 0x202020,
   wireframe: true,
- });
+});
 const cube = new THREE.Mesh(geo, mat);
 globeGroup.add(cube);
 
-const detail = 120;
+const detail = 128;
 const pointsGeo = new THREE.IcosahedronGeometry(1, detail);
 
-const vertexShader = `
+const vertexShader = /*glsl*/ `
   uniform float size;
   uniform sampler2D elevTexture;
 
@@ -51,31 +100,63 @@ const vertexShader = `
     gl_Position = projectionMatrix * mvPosition;
   }
 `;
-const fragmentShader = `
+const fragmentShader = /*glsl*/ `
   uniform sampler2D colorTexture;
   uniform sampler2D alphaTexture;
+  uniform sampler2D earthTexture;
+  uniform sampler2D hexagonTexture;
 
   varying vec2 vUv;
   varying float vVisible;
 
+  float sdHexagon( in vec2 p, in float r )
+  {
+    const vec3 k = vec3(-0.866025404,0.5,0.577350269);
+    p = abs(p);
+    p -= 2.0*min(dot(k.xy,p),0.0)*k.xy;
+    p -= vec2(clamp(p.x, -k.z*r, k.z*r), r);
+    return length(p)*sign(p.y);
+  }
+
+  float opOnion( in vec2 p, in float r )
+  {
+    return abs(sdHexagon(p,r)) - r;
+  }
+
   void main() {
     if (floor(vVisible + 0.1) == 0.0) discard;
+
+    float spriteAlpha = texture2D(hexagonTexture, gl_PointCoord).a;
+
+    // 如果距离大于0（即片元在圆形外部），丢弃该片元
+    if (spriteAlpha<0.1) discard;
+
+    // 计算当前片元的颜色
     float alpha = 1.0 - texture2D(alphaTexture, vUv).r;
     vec3 color = texture2D(colorTexture, vUv).rgb;
-    gl_FragColor = vec4(color, alpha);
+    vec3 earth = texture2D(earthTexture, vUv).rgb;
+    color = mix(color, earth, 0.75);
+
+    if (alpha > 0.5) {
+      gl_FragColor = vec4(color, alpha*spriteAlpha);
+    }else {
+      gl_FragColor = vec4(earth, 1.0*spriteAlpha);
+    }
   }
 `;
 const uniforms = {
   size: { type: "f", value: 4.0 },
   colorTexture: { type: "t", value: colorMap },
   elevTexture: { type: "t", value: elevMap },
-  alphaTexture: { type: "t", value: alphaMap }
+  alphaTexture: { type: "t", value: alphaMap },
+  earthTexture: { type: "t", value: earthMap },
+  hexagonTexture: { type: "t", value: hexagonMap },
 };
 const pointsMat = new THREE.ShaderMaterial({
   uniforms: uniforms,
   vertexShader,
   fragmentShader,
-  transparent: true
+  transparent: true,
 });
 
 const points = new THREE.Points(pointsGeo, pointsMat);
@@ -84,22 +165,26 @@ globeGroup.add(points);
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x080820, 3);
 scene.add(hemiLight);
 
-const stars = getStarfield({ numStars:4500, sprite: starSprite });
+const stars = getStarfield({ numStars: 4500, sprite: starSprite });
 scene.add(stars);
 
 function animate() {
   renderer.render(scene, camera);
-  globeGroup.rotation.y += 0.002;
+  // globeGroup.rotation.y += 0.002;
 
   requestAnimationFrame(animate);
   orbitCtrl.update();
-};
+}
 animate();
 
-window.addEventListener('resize', function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}, false);
+window.addEventListener(
+  "resize",
+  function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  },
+  false
+);
 
 // https://discourse.threejs.org/t/earth-point-vertex-elevation/62689
